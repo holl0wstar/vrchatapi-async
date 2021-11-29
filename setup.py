a646eb6db1a6887c457c0c7eefa6fc1edd77e6bc
Henrique Cezar Godoy Bueno <theholl0wstar@gmail.com>
2021-11-29 14:06:01 -0300

    First commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+NAME = 'VRChatAPI-Async'
+VERSION = '0.0.1'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author='Skyelight',
+    author_email='distortedinferno@gmail.com',
+    description='An async version of the VRChat API Library.',
+    long_description=description,
+    python_requires='>=3.8',
+)
